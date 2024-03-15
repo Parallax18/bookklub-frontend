c24ed23fda9cf1150117b485596b92878f557db0
outis013 <kanieloutis8945@gmail.com>
2024-03-15 17:59:00 +0100

    added another theme variant-- please let me know which is right

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,5 +1,43 @@
 import { extendTheme } from "@chakra-ui/react";
 
+// 2. Call `extendTheme` and pass your custom values
+const customTheme = extendTheme({
+  colors: {
+    400: "#599B7B",
+    100: "##ACCDBD",
+    50: "#DEEBE5",
+    error: "#E26E6A",
+    success: "#5FC381",
+    "shade-black": "#1B1C1E",
+    "shade-white": "#FAF9F6",
+    "grey-600": "#475367",
+    "grey-400": "#98A2B3",
+  },
+
+  bg: {
+    "bg-color": "#1B1C1EE5",
+    "dark-bg": "#121212",
+    white: "#ffffff",
+  },
+
+  buttons: {
+    disabled: "#90BCA7",
+    bg: "#599B7B",
+    text: "#FFFFFF",
+  },
+
+  icons: {
+    active: "#599B7B",
+    inactive: "#98A2B3",
+    width: "18px",
+    height: "20px",
+  },
+});
+
+
+
+
+
 export const theme = extendTheme({
   colors: {
     "button-color-active": "#599B7B",
@@ -22,7 +60,23 @@ export const theme = extendTheme({
     padding: "0px, 16px",
     jusitfy: "space-between",
     border: "1px, 0px, 0px, 0px",
-    "border-radius": "8px, 8px, 0px, 0px"
+    "border-radius": "8px, 8px, 0px, 0px",
+    button: {
+      gap: "8px",
+      padding: "0px, 8px, 8px, 8px",
+      "icon-container": {
+        gap: "4px",
+        width: "33px",
+        height: "45px",
+      },
+
+      icon: {
+        width: "19.75px",
+        height: "18.75px",
+        top: "3.25px",
+        left: "2.13px",
+      },
+    },
   },
 
   auth: {
