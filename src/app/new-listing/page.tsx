99dfd90061e7dc6b5dfbd3f02938e5cb6fdc682c
psalmseen <samsonoyebamiji02@gmail.com>
2024-03-30 12:13:17 +0100

    working on the authpagefooter component

---
+++ src/app/new-listing/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import AuthPagesFooterComponent from '@/components/AuthPagesFooterComponent';
+
+export default function newListing() {
+  return (
+    <main style={{ color: '#fff' }}>
+      <AuthPagesFooterComponent></AuthPagesFooterComponent>
+    </main>
+  );
+}
---
+++ src/components/AuthPagesFooterComponent.tsx
@@ -0,0 +1,17 @@
+import { Flex, Spacer, Box } from '@chakra-ui/react';
+
+export default function AuthPagesFooterComponent() {
+  return (
+    <Flex>
+      <Box>Home</Box>
+      <Spacer />
+      <Box>Rentals</Box>
+      <Spacer />
+      <Box>List a book</Box>
+      <Spacer />
+      <Box>notifications</Box>
+      <Spacer />
+      <Box>Messages</Box>
+    </Flex>
+  );
+}
