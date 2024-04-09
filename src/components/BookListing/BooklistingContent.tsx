c560670e0cd2a60f284f516b7fe58fd2adc575d1
psalmseen <samsonoyebamiji02@gmail.com>
2024-04-10 00:40:21 +0100

    feat: added the image display and the upload utility function

--- src/components/BookListing/BooklistingContent.tsx
+++ src/components/BookListing/BooklistingContent.tsx
@@ -4,36 +4,37 @@ import { Box, Stack, Button } from '@chakra-ui/react';
 import ReusableInput from '../general/Input';
 import ReusableTextarea from '../general/ReusableTextArea';
 import PictureUpload from './PictureUploadComponent';
-
-const BookListingContent = ({ formik }: { formik: any }) => {
+import { useFormikContext } from 'formik';
+const BookListingContent = () => {
+  const { handleSubmit, values }: any = useFormikContext();
   return (
-    <form onSubmit={formik.handleSubmit}>
+    <form onSubmit={handleSubmit}>
       <Box>
         <Stack spacing={'16px'}>
           <ReusableInput
             label="Book name"
-            value={formik.values.bookName}
+            value={values.bookName}
             placeholder="eg. Think and grow rich"
             type="text"
             name="bookName"
           />
           <ReusableInput
             label="Author name"
-            value={formik.values.authorName}
+            value={values.authorName}
             placeholder="e.g. Napoleon Hill"
             type="text"
             name="authorName"
           />
           <ReusableInput
             label="Book location"
-            value={formik.values.bookLocation}
+            value={values.bookLocation}
             placeholder="e.g. 23, Langley Crescent, Idumota, Lagos"
             type="text"
             name="bookLocation"
           />
           <ReusableInput
             label="Rent duratiom"
-            value={formik.values.rentDuration.toString()}
+            value={values.rentDuration.toString()}
             placeholder="e.g. 30"
             type="number"
             name="rentDuration"
@@ -42,7 +43,7 @@ const BookListingContent = ({ formik }: { formik: any }) => {
           />
           <ReusableTextarea
             label="Rent info (optional)"
-            value={formik.values.rentInfo}
+            value={values.rentInfo}
             placeholder="Give an extra info that would be useful for renters"
             name="rentInfo"
             description="Provide any extra information, note, caution that anyone renting this book should know"
@@ -51,6 +52,7 @@ const BookListingContent = ({ formik }: { formik: any }) => {
             label="Add book cover"
             placeholder="Upload the book cover here"
             maxFileSizeInMB="2"
+            description="This is the book cover that would be displayed. Ensure you select a picture that support the book name and is of high quality"
           />
         </Stack>
       </Box>
